c50fd9a624146d7375e8907963e76634b293d28e
SepVeneto <sepveneto@qq.com>
2023-03-01 10:06:26 +0800

    fix: component type any

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,15 +7,8 @@ pnpm i --frozen-lockfile --registry=https://registry.npmmirror.com
 
 pnpm build
 
-echo "log npmrc"
-cat ./.npmrc
-echo "log button.d.ts"
-cat dist/types/packages/components/button/src/button.d.ts
-
-asdf;asf;sad;fkasd asfksiifesf
-
-#cd dist/basic-comp
-#sed -i "s/\"version\": \"1.0.0\",/\"version\": \"$TAG_VERSION\",/" package.json
-#pnpm publish --no-git-checks --access public
+cd dist/basic-comp
+sed -i "s/\"version\": \"1.0.0\",/\"version\": \"$TAG_VERSION\",/" package.json
+pnpm publish --no-git-checks --access public
 
 echo "✅ Publish completed"
