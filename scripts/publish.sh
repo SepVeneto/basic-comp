e15db27a96dc6f41d81731ba4f938dc2d37703ea
SepVeneto <sepveneto@qq.com>
2023-03-01 09:54:37 +0800

    ci:  debug gen type

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,6 +9,8 @@ pnpm build
 
 cat dist/types/packages/components/button/src/button.d.ts
 
+asdf;asf;sad;fkasd asfksiifesf
+
 #cd dist/basic-comp
 #sed -i "s/\"version\": \"1.0.0\",/\"version\": \"$TAG_VERSION\",/" package.json
 #pnpm publish --no-git-checks --access public
