9ec9294cf730ffd9dff2c9c0c49bc7ad37958164
SepVeneto <sepveneto@qq.com>
2023-03-01 09:45:39 +0800

    ci: debug workflow

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,9 +7,10 @@ pnpm i --frozen-lockfile --registry=https://registry.npmmirror.com
 
 pnpm build
 
+cat dist/types/packages/components/button/src/button.d.ts
 
-cd dist/basic-comp
-sed -i "s/\"version\": \"1.0.0\",/\"version\": \"$TAG_VERSION\",/" package.json
-pnpm publish --no-git-checks --access public
+#cd dist/basic-comp
+#sed -i "s/\"version\": \"1.0.0\",/\"version\": \"$TAG_VERSION\",/" package.json
+#pnpm publish --no-git-checks --access public
 
 echo "✅ Publish completed"
