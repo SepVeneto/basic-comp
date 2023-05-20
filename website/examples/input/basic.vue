1711354457d6a54344ac4b83e0a3ab86878e5ab0
SepVeneto <sepveneto@qq.com>
2023-05-20 17:24:43 +0800

    fix(input): border no effect

--- website/examples/input/basic.vue
+++ website/examples/input/basic.vue
@@ -2,14 +2,10 @@
   <bc-input v-model="text" />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
-  name: 'Basic',
-})
-</script>
-
 <script lang="ts" setup>
 import { ref } from 'vue'
+defineOptions({
+  name: 'Basic',
+})
 const text = ref('')
 </script>
