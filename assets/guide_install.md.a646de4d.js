971ff6e1cc449d10db6aa9632dac9e5e9b1884ff
SepVeneto <SepVeneto@users.noreply.github.com>
2023-06-18 02:04:36 +0000

    Deploying to docs from @ SepVeneto/basic-comp@946fd282b866cadf6af7568196b9fbadee2ca895 🚀

---
+++ assets/guide_install.md.a646de4d.js
@@ -0,0 +1,16 @@
+import{_ as e,o as n,c as t,h as a}from"./app.b8ea2ab0.js";const g='{"title":"Install","description":"","frontmatter":{"title":"Install"},"headers":[{"level":2,"title":"\u73AF\u5883\u652F\u6301","slug":"\u73AF\u5883\u652F\u6301"},{"level":2,"title":"\u672C\u5730\u5B89\u88C5","slug":"\u672C\u5730\u5B89\u88C5"},{"level":2,"title":"\u4F7F\u7528\u5305\u7BA1\u7406\u5668\u5B89\u88C5","slug":"\u4F7F\u7528\u5305\u7BA1\u7406\u5668\u5B89\u88C5"}],"relativePath":"guide/install.md"}',s={},o=a(`<h1 id="\u5B89\u88C5" tabindex="-1">\u5B89\u88C5 <a class="header-anchor" href="#\u5B89\u88C5" aria-hidden="true">#</a></h1><h2 id="\u73AF\u5883\u652F\u6301" tabindex="-1">\u73AF\u5883\u652F\u6301 <a class="header-anchor" href="#\u73AF\u5883\u652F\u6301" aria-hidden="true">#</a></h2><p>\u7531\u4E8E\u662F\u5BF9<code>element-plus</code>\u7684\u4E8C\u6B21\u5C01\u88C5\uFF0C\u540C\u65F6\u57FA\u4E8E<code>vue3</code>\uFF0C\u56E0\u6B64\u4E0D\u652F\u6301ie11</p><table><thead><tr><th><img src="https://cdn.jsdelivr.net/npm/@browser-logos/edge/edge_32x32.png" alt=""></th><th><img src="https://cdn.jsdelivr.net/npm/@browser-logos/firefox/firefox_32x32.png" alt=""></th><th><img src="https://cdn.jsdelivr.net/npm/@browser-logos/chrome/chrome_32x32.png" alt=""></th><th><img src="https://cdn.jsdelivr.net/npm/@browser-logos/safari/safari_32x32.png" alt=""></th></tr></thead><tbody><tr><td>Edge \u2265 79</td><td>Firefox \u2265 78</td><td>Chrome \u2265 64</td><td>Safari \u2265 12</td></tr></tbody></table><h2 id="\u672C\u5730\u5B89\u88C5" tabindex="-1">\u672C\u5730\u5B89\u88C5 <a class="header-anchor" href="#\u672C\u5730\u5B89\u88C5" aria-hidden="true">#</a></h2><div class="language-bash"><pre><code><span class="token function">git</span> clone https://github.com/SepVeneto/basic-comp.git
+
+<span class="token function">pnpm</span> i
+
+<span class="token function">pnpm</span> build:comps
+
+<span class="token comment"># \u6216\u8005\u901A\u8FC7npm link\u5EFA\u7ACB\u8F6F\u8FDE\u63A5</span>
+</code></pre></div><h2 id="\u4F7F\u7528\u5305\u7BA1\u7406\u5668\u5B89\u88C5" tabindex="-1">\u4F7F\u7528\u5305\u7BA1\u7406\u5668\u5B89\u88C5 <a class="header-anchor" href="#\u4F7F\u7528\u5305\u7BA1\u7406\u5668\u5B89\u88C5" aria-hidden="true">#</a></h2><div class="language-bash"><pre><code><span class="token comment"># npm</span>
+<span class="token function">npm</span> i @sepveento/basic-comp
+
+<span class="token comment">#yarn</span>
+<span class="token function">yarn</span> <span class="token function">add</span> @sepveento/basic-comp
+
+<span class="token comment">#pnpm</span>
+<span class="token function">pnpm</span> i @sepveento/basic-comp
+</code></pre></div>`,8),c=[o];function r(i,p,d,l,h,m){return n(),t("div",null,c)}var f=e(s,[["render",r]]);export{g as __pageData,f as default};
